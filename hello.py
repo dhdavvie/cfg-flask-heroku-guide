2574193b5b76338c6cf54a75a2b3ca46da9323f1
David Heiberg <dhdavvie@gmail.com>
2019-04-09 19:34:16 +0100

    updated for port binding

--- hello.py
+++ hello.py
@@ -16,4 +16,5 @@ def sign_up():
     return "All OK"
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
